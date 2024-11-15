a695199eee2b6c4f72318486f91aaaff2bd1fdf5
theghost99 <bobs.best99@gmail.com>
2024-11-15 22:36:50 +0000

    Original creation of new project

---
+++ StardewUnattendedServer/Framework/ModData.cs
@@ -0,0 +1,17 @@
+﻿namespace StardewUnattendedServer.Framework
+{
+    class ModData
+    {
+        public int FarmingLevel { get; set; }
+        public int MiningLevel { get; set; }
+        public int ForagingLevel { get; set; }
+        public int FishingLevel { get; set; }
+        public int CombatLevel { get; set; }
+        public int FarmingExperience { get; set; }
+        public int MiningExperience { get; set; }
+        public int ForagingExperience { get; set; }
+        public int FishingExperience { get; set; }
+        public int CombatExperience { get; set; }
+
+    }
+}
